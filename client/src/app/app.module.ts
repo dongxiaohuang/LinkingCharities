a6068878f1433181ba7d298b1bd73a40cae8d8ae
dongxiaohuang <dongxiao.huang17@imperial.ac.uk>
2018-06-04 21:17:07 +0100

    initial web site and make use of font font-family: Roboto, sans-serif;

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,14 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
