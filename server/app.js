83076d03d9098053d27d1189e5281f947ff62f9d
dongxiaohuang <dongxiao.huang17@imperial.ac.uk>
2018-06-19 15:53:40 +0100

    set up express server

--- server/app.js
+++ server/app.js
@@ -9,6 +9,18 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+// redirect to secure ports
+app.all('*', (req, res, next) =>{
+     // request coming through secure port
+     if(req.secure){
+          next();//do nothing
+     }
+     else{
+          res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url);
+     }
+});
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -19,7 +31,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+// app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
