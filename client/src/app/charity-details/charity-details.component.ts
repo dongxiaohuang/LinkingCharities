fe3cc85672a3421c61655f0f0abb6602d91ccd47
dongxiaohuang <dongxiao.huang17@imperial.ac.uk>
2018-07-01 12:57:15 +0100

    UI for teamworkers and change icon fonts of footer

--- client/src/app/charity-details/charity-details.component.ts
+++ client/src/app/charity-details/charity-details.component.ts
@@ -3,22 +3,29 @@ import { GetCharitiesService } from '../services/get-charities.service';
 import { Charity } from '../shared/charity';
 import { Params, ActivatedRoute } from '@angular/router';
 import { baseURL } from '../shared/baseurl';
+import { NgbRatingConfig } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-charity-details',
   templateUrl: './charity-details.component.html',
-  styleUrls: ['./charity-details.component.scss']
+  styleUrls: ['./charity-details.component.scss'],
+  providers: [NgbRatingConfig] // add NgbRatingConfig to the component providers
 })
 export class CharityDetailsComponent implements OnInit {
 
   charity: Charity;
+  currentRate: number = 4;
   baseUrl: string = baseURL;
   constructor(private charityService: GetCharitiesService,
-       private route: ActivatedRoute) { }
+    private route: ActivatedRoute,
+     config: NgbRatingConfig) {
+          config.max = 5;
+     }
 
   ngOnInit() {
-       let id = this.route.snapshot.params['id']; //'+' convert a string into interger value
-       this.charityService.getCharity(id)
-          .subscribe(charity => this.charity = charity);
+    let id = this.route.snapshot.params['id']; //'+' convert a string into interger value
+    this.charityService.getCharity(id)
+      .subscribe(charity => this.charity = charity);
   }
+  selected = 0;
 
 }
