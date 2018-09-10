7dee9a7c2e462c317e422f0c4908cddb3d8de678
dongxiaohuang <32898923+dongxiaohuang@users.noreply.github.com>
2018-09-10 21:34:19 +0100

    Delete config.ts

--- client/src/app/config.ts
+++
@@ -1,3 +0,0 @@
-export const googleAPI: string = "AIzaSyDgYITwFa8Y3SzcrRHKpgDJEEntuj65le8";
-export const stripeAPI: string = 'pk_test_nsptMFRRApES3nRXJFic53XJ';
-export const facebookAppID: string = '188557768477636';
